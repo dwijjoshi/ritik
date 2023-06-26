fd1c448d419a4d4fdd3ed0db31b1cf46da20c792
Your Name <Your Email>
2023-06-27 00:56:59 +0530

    first commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
-var mongoose = require ('mongoose');
-
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
-
-module.exports = {mongoose};
+mongoose.connect(
+  "mongodb+srv://dwijjoshi02:IyLQ1ph3tfFab0Uq@hospital.sqza9z7.mongodb.net/hospi?retryWrites=true&w=majority"
+);
+//
+module.exports = { mongoose };
